89ea2d047629df02e7852fa1c767fc75c129cdc1
emall <estelle.mallozzi.santamaria@gmail.com>
2018-11-27 11:15:14 +0100

    Modification Estelle2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::get('/getLogin', 'VisiteurController@getLogin');
+Route::post('/login', 'VisiteurController@signIn');
+Route::get('/getLogout', 'VisiteurController@signOut');
